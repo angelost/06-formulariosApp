28a45d241a25da476a368870dd2f210e2d75bd76
Angelo <angelo.gonzalez@live.com>
2022-09-01 13:41:10 -0400

    261. Actualizar el valor de la persona

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -28,6 +28,16 @@ export class SwitchesComponent implements OnInit {
       ...this.persona,
       condiciones: false
     } );
+
+    this.miFormulario.valueChanges.subscribe( ({condiciones, ...rest }) => {
+      this.persona = rest;
+    })
+  }
+
+  guardar() {
+    const formValue = { ...this.miFormulario.value };
+    delete formValue.condiciones;
+    this.persona = formValue;
   }
   
 
