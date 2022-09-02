9ae59d89717f8844fefc25c4708c7e465fc7a46b
Angelo <angelo.gonzalez@live.com>
2022-09-02 14:58:42 -0400

    275. Validaciones asíncronas

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { emailPattern, nombreApellidoPatern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones';
 import { ValidatorService } from '../../services/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -13,7 +14,7 @@ export class RegistroComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [ Validators.required, Validators.pattern( this.vs.nombreApellidoPatern ) ] ],
-    email: ['', [ Validators.required, Validators.pattern( this.vs.emailPattern ) ] ],
+    email: ['', [ Validators.required, Validators.pattern( this.vs.emailPattern ) ], [ this.emailValidator ]],
     username: ['', [ Validators.required, this.vs.noPuedeSerStrider ] ],
     password: ['', [ Validators.required, Validators.minLength(6) ] ],
     password2: ['', [ Validators.required ] ]
@@ -22,12 +23,13 @@ export class RegistroComponent implements OnInit {
   })
 
   constructor( private fb: FormBuilder,
-               private vs: ValidatorService ) { }
+               private vs: ValidatorService,
+               private emailValidator: EmailValidatorService ) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Angelo Gonzalez',
-      email: 'angelo.gonzalez@live.com',
+      email: 'test1@test.com',
       username: 'angelost86'
     })
   }
