930647ae0aee28070acdb72002efff1915c5285c
Angelo <angelo.gonzalez@live.com>
2022-09-01 20:52:44 -0400

    272. Separar la lógica de validaciones

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { emailPattern, nombreApellidoPatern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones';
+import { ValidatorService } from '../../services/validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,30 +9,16 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
   styles: [
   ]
 })
-export class RegistroComponent implements OnInit {
-
-  nombreApellidoPatern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
-  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
-
-  noPuedeSerStrider( control: FormControl ) {
-    const valor: string = control.value?.trim().toLowerCase();
-    if( valor === 'strider') {
-      return {
-        noStrider: true
-      }
-    }
-
-    return null;
-  
-  }
+export class RegistroComponent implements OnInit {    
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', [ Validators.required, Validators.pattern( this.nombreApellidoPatern ) ] ],
-    email: ['', [ Validators.required, Validators.pattern( this.emailPattern ) ] ],
-    username: ['', [ Validators.required, this.noPuedeSerStrider ] ]
+    nombre: ['', [ Validators.required, Validators.pattern( this.vs.nombreApellidoPatern ) ] ],
+    email: ['', [ Validators.required, Validators.pattern( this.vs.emailPattern ) ] ],
+    username: ['', [ Validators.required, this.vs.noPuedeSerStrider ] ]
   })
 
-  constructor( private fb: FormBuilder ) { }
+  constructor( private fb: FormBuilder,
+               private vs: ValidatorService ) { }
 
   ngOnInit(): void {
     this.miFormulario.reset({
